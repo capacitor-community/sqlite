e8ac33788948ff801267ec4efa0eedddb3528546
Robin Genz <mail@robingenz.dev>
2026-03-18 08:52:09 +0100

    Fixes

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorCommunitySqlite",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorCommunitySqlite",
